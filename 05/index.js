cf199f493741e22c2aa181e67364d72229cb5566
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-10 20:28:39 -0300

    Add resolução do exercício 05

--- 05/index.js
+++ 05/index.js
@@ -1,5 +1,12 @@
 // Crie sua função aqui
-
+function evenArray(number) {
+    const array = []
+    if (number <= 0) return array
+    for (let i = 0; i <= number; i++) {
+      if (i % 2 === 0) array.push(i)
+    }
+    return array
+  }
 
 // Não altere as linhas abaixo
 console.log(evenArray(10))
