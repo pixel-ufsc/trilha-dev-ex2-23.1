84db02ef27163a05358cc2cfc8f497d882421f1b
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-10 20:52:07 -0300

    Add resolução do exercício 06

--- 06/index.js
+++ 06/index.js
@@ -1,5 +1,13 @@
 // Crie sua função aqui
-
+function factorial(num){
+    if (num < 0){
+        return -1;
+    } else if (num == 0){
+        return 1;
+    } else {
+        return (num * factorial(num - 1));
+    }
+}
 
 // Não altere as linhas abaixo
 console.log(factorial(0))
