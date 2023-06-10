c16de057584f4cbb667f989e0c58983432877155
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-10 17:37:57 -0300

    Add resolução exercício 02

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,8 @@
 // Crie sua função aqui
-
+function invertWord(a){
+    let inverted = a.split("").reverse().join("")
+    return inverted
+}
 
 // Não altere as linhas abaixo
 console.log(invertWord('javascript'))
