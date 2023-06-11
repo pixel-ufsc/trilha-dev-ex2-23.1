2654fd924bf3c1a1883d4661c03b355cd44cc6fa
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-11 11:42:08 -0300

    Add resolução do exercício 08

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,11 @@
 // Crie sua função aqui
-
+function uppercaseArray(list){
+    let newList = [];
+    for(let i = 0; i < list.length; i++){
+        newList.push(list[i].toUpperCase());
+    }
+    return newList
+}
 
 // Não altere as linhas abaixo
 console.log(uppercaseArray(['pixel', 'Trainee', 'trilha dev']))
