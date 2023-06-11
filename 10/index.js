5d57d9a5c6e45cad6385d502744ba40b110bcafd
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-11 12:25:37 -0300

    Add resolução do exercício 10

--- 10/index.js
+++ 10/index.js
@@ -1,5 +1,10 @@
 // Crie sua função aqui
-
+function secondBiggestNumber(numbers) {
+    if (!Array.isArray(numbers)) return null
+    if (numbers.length <= 1) return null
+    numbers.sort((a, b) => b - a)
+    return numbers[1]
+  }
 
 // Não altere as linhas abaixo
 console.log(secondBiggestNumber([0, 1, 2, 3, 4, 5]))
