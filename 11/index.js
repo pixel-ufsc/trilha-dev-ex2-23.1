a6affc784cf2a0c8a2000f4b4916a950a9f68d9b
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-11 14:02:24 -0300

    Add resolução do exercício 11

--- 11/index.js
+++ 11/index.js
@@ -1,5 +1,13 @@
 // Crie sua função aqui
-
+function filterContainsA(list){
+  newList = []
+  for (let i = 0; i < list.length; i++){
+    if (list[i].includes('a') || list[i].includes('A')){
+      newList.push(list[i])
+    }
+  }
+  return newList
+}
 
 // Não altere as linhas abaixo
 console.log(filterContainsA([
