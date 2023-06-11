8015069e336c3ea21d3cc1e8a98692f5668e333c
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-06-11 11:33:06 -0300

    Add resolução do exercício 07

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,11 @@
 // Crie sua função aqui
-
+function sumArray(a){
+    let total = 0;
+    for (let i = 0; i < a.length; i++){
+        total = total + a[i];
+    }
+    return total;
+}
 
 // Não altere as linhas abaixo
 console.log(sumArray([0, 1, 2, 3, 4, 5]))
